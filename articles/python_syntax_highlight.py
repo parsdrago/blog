2752b7c02e1fe0e45a99b7f36d5365f3725f3b75
utan <iw.utan@gmail.com>
2024-01-06 23:41:51 +0900

    add article

---
+++ articles/python_syntax_highlight.py
@@ -0,0 +1,6 @@
+# Python でのシンタックスハイライト
+
+[Pygments](https://pygments.org/) というものが有用そうです。
+普通の Python ライブラリか CLI ツールとして利用でき、多様な言語のコードをシンタックスハイライトした html と css に変換してくれるようです。
+
+Python-markdown からも extension として利用可能になっているそうで、自作のツールにも容易に組み込めそうです。
